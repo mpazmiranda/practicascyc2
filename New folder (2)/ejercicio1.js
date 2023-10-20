b58ff34785a886f6f9aec8a62c75ec2c4ddbe7fe
mpazmiranda <124013176+mpazmiranda@users.noreply.github.com>
2023-10-20 16:56:54 -0300

    Create ejercicio1.js

---
+++ New folder (2)/ejercicio1.js
@@ -0,0 +1,9 @@
+function ParImpar(numero) {
+    var division = numero%2;
+    if (division == 0) {
+        console.log (numero+ " es par" ); 
+    } else {console.log (numero+ " es impar" ) };
+ }
+
+ ParImpar (6);
+ ParImpar (11);
